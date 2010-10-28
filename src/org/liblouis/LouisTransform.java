ff400e40ed784997553c6288584c237db55ba5e9
Bernhard Wagner <bernhard.wagner@sbs.ch>
2010-10-28 09:29:11 +0200

    CHANGED: improved how to add my extension, namely using setFactoryConfiguration.

--- src/org/liblouis/LouisTransform.java
+++ src/org/liblouis/LouisTransform.java
@@ -1,39 +1,31 @@
 package org.liblouis;
 
-import net.sf.saxon.Configuration;
 import net.sf.saxon.Transform;
 import net.sf.saxon.trans.XPathException;
 
 public class LouisTransform extends Transform {
-	
-	private void addExtension(){
+
+	private void addExtension() {
 		try {
 			getConfiguration().registerExtensionFunction(
 					new LouisExtensionFunctionDefinition());
 		} catch (final XPathException e) {
-			throw new RuntimeException("failed adding extension:",e);
-		}		
+			throw new RuntimeException("failed adding extension:", e);
+		}
 	}
-	
-	private void configStuff(){
-		config = Configuration.newConfiguration();
-		config.setVersionWarning(true);
-        try {
-            setFactoryConfiguration(false, null);
-        } catch (final Exception err) {
-            err.printStackTrace();
-            System.err.println(err.getMessage());
-            System.exit(2);
-        }
+
+	@Override
+	public void setFactoryConfiguration(boolean schemaAware, String className)
+			throws RuntimeException {
+		super.setFactoryConfiguration(schemaAware, className);
+		addExtension();
 	}
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		LouisTransform myt = new LouisTransform();
-		myt.configStuff();
-		myt.addExtension();
-		myt.doTransform(args, "java org.liblouis.LouisTransform");
+		new LouisTransform().doTransform(args, "java org.liblouis.LouisTransform");
 	}
 
 }
